b83cfc40fc5bad273ce36b0e4830eb811ebfe92c
Jonathan Dahan <hi@jonathan.is>
2019-02-13 18:47:27 -0500

    install pitft

---
+++ share/scripts/05_install_pitft.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+cd ~
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/adafruit-pitft.sh
+chmod +x adafruit-pitft.sh
+./adafruit-pitft.sh
