ace33eebe95209a98757d738843492588ff9d223
Jonathan Dahan <hi@jonathan.is>
2019-03-02 12:10:36 -0500

    hide broken things

--- share/root/scripts/00_update.bash
+++ share/root/scripts/00_update.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -ex
 
+# enable iptables - we do this before upgrading because kernel might be upgraded
+modprobe ip_tables
+grep -q 'ip_tables' /etc/modules || echo 'ip_tables' >> /etc/modules
+
 DEBIAN_FRONTEND=noninteractive apt update
 DEBIAN_FRONTEND=noninteractive apt upgrade -y
