23770b8754379ac35f3c8fe9070fe16784e3c9be
Jonathan Dahan <hi@jonathan.is>
2019-03-06 17:57:55 -0500

    pass in NPM_CONFIG_PREFIX

--- share/root/scripts/00_install_npm.bash
+++ share/root/scripts/00_install_npm.bash
@@ -2,13 +2,12 @@
 
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 
-export NPM_CONFIG_PREFIX=/home/pi/npm
-sudo -u pi mkdir -p $NPM_CONFIG_PREFIX
-grep NPM_CONFIG_PREFIX /etc/environment || echo NPM_CONFIG_PREFIX="$NPM_CONFIG_PREFIX" >> /etc/environment
+grep -q NPM_CONFIG_PREFIX /etc/environment || echo NPM_CONFIG_PREFIX="/home/pi/npm" >> /etc/environment
+grep -q PATH /etc/environment || echo PATH="$NPM_CONFIG_PREFIX/bin:$PATH" >> /etc/environment
+source /etc/environment
 
-export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
-grep PATH /etc/environment || echo PATH="$PATH" >> /etc/environment
+sudo -u pi NPM_CONFIG_PREFIX=$NPM_CONFIG_PREFIX mkdir -p $NPM_CONFIG_PREFIX
 
 require npm nodejs
 
-sudo -u pi npm install -g dat
+sudo -u pi NPM_CONFIG_PREFIX=$NPM_CONFIG_PREFIX npm install --global --unsafe-perm dat
