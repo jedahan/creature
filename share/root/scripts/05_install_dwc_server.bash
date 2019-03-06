200957960cb5d91cb9ed8f605d429b4b2f145c19
Jonathan Dahan <hi@jonathan.is>
2019-03-06 18:17:50 -0500

    add requirements

--- share/root/scripts/05_install_dwc_server.bash
+++ share/root/scripts/05_install_dwc_server.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+require git
+require npm
+
 sudo -u pi git clone https://github.com/CezarMocan/dwc-creature-server.git /home/pi/dwc-creature-server
 cd /home/pi/dwc-creature-server
 sudo -i pi npm install
