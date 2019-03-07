b78a320b8d6c1f7ee0d3858a7ea2f7388e4ed284
Jonathan Dahan <hi@jonathan.is>
2019-03-06 20:00:32 -0500

    user pi

--- share/root/scripts/06_install_dwc_server.bash
+++ share/root/scripts/06_install_dwc_server.bash
@@ -6,4 +6,4 @@ require npm nodejs
 
 sudo -u pi git clone https://github.com/CezarMocan/dwc-creature-server.git /home/pi/dwc-creature-server
 cd /home/pi/dwc-creature-server
-sudo -i pi npm install
+sudo -u pi npm install
