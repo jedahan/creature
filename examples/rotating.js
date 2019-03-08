48d5471fd7aafc765371dad5662f21c7863c8b3d
Jonathan Dahan <hi@jonathan.is>
2019-03-08 13:15:57 -0500

    add creature wrapper example

---
+++ examples/rotating.js
@@ -0,0 +1,16 @@
+import ascii from 'ascii-faces'
+import { getPath } from '../src/getPath'
+import { Creature } from '../src/Creature'
+const initialFace = 'what face'
+const { filePath, datPath } = getPath()
+const creature = new Creature({ initialFace, filePath, datPath })
+creature.on('connection', console.dir)
+creature.on('update', console.dir)
+
+creature.on('created', () => {
+  setInterval(() => {
+    creature.update({ creature: ascii() })
+  }, 5000)
+})
+
+creature.sync()
